b9c8549ec639dd24632c61f4ca82ae7700391ab7
Adam Hsu <projectnba77@gmail.com>
2022-06-10 17:34:56 +0800

    fix: user API 1.調整 Route 路徑 2.重設密碼 請求方法

--- routes/users.js
+++ routes/users.js
@@ -7,22 +7,22 @@ const handleErrorAsync = require("../service/handleErrorAsync")
 const { isAuth } = require('../service/isAuth')
 
 // 取得所有用戶
-router.get('/', isAuth, handleErrorAsync(UserController.getUser))
+router.get('/users', isAuth, handleErrorAsync(UserController.getUser))
 
 // 用戶登入
-router.post('/singin', handleErrorAsync(UserController.singin))
+router.post('/users/sing_in', handleErrorAsync(UserController.singin))
 
 // 用戶註冊
-router.post('/singup', handleErrorAsync(UserController.singup))
+router.post('/users/sing_up', handleErrorAsync(UserController.singup))
 
 // 重設密碼
-router.post('/updatePassword', isAuth, handleErrorAsync(UserController.updatePassword))
+router.patch('/users/updatePassword', isAuth, handleErrorAsync(UserController.updatePassword))
 
 // 取得個人資料
-router.get('/profile', isAuth, handleErrorAsync(UserController.getProfile))
+router.get('/users/profile', isAuth, handleErrorAsync(UserController.getProfile))
 
 // 更新個人資料
-router.patch('/profile', isAuth, handleErrorAsync(UserController.updateProfile))
+router.patch('/users/profile', isAuth, handleErrorAsync(UserController.updateProfile))
 
 
 module.exports = router
