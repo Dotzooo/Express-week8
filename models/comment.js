02461cf66b638d62d6e6477b1e9767ff3c67e7f5
Adam Hsu <projectnba77@gmail.com>
2022-06-12 23:57:28 +0800

    feat: comment Model

---
+++ models/comment.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+// 建立 Schema
+const commentSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User",
+            required: [true, "請填寫創作者 ID"],
+        },
+        comment: {
+            type: String,
+            trim: true,
+            required: [true, "請填寫留言內容"],
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        updatedAt: {
+            type: Date,
+            default: Date.now,
+        },
+        logicDeleteFlag: {
+            type: Boolean,
+            default: false,
+            select: false,
+        },
+    },
+    {
+        versionKey: false,
+    }
+)
+
+// 建立 Model
+const Comment = mongoose.model("Comment", commentSchema)
+
+module.exports = Comment
